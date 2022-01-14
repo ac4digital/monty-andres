07f7ba6cbc871d30375b29e97a5c49fd8f3fe4c6
Andrés Cataño <3866@holbertonschool.com>
2022-01-13 23:47:35 -0500

    Fix nop

--- fn1.c
+++ fn1.c
@@ -73,6 +73,6 @@ void pint(stack_t **stack, unsigned int line_number)
 
 void nop(stack_t **stack, unsigned int line_number)
 {
-	stack = stack;
-	line_number = line_number;
+	(void) stack;
+	(void) line_number;
 }

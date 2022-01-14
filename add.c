701fd73187f8acfebf082b0d0567ea3787fa5fca
Andrés Cataño <3866@holbertonschool.com>
2022-01-14 08:23:12 -0500

    Fix Betty Styles

--- add.c
+++ add.c
@@ -2,12 +2,12 @@
 
 /**
  * add - adds the top two elements of the stack.
- * @head: head (stack) to the stack;
+ * @stack: head (stack) to the stack;
  * @line_number: line number where opcode is located
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = *stack;
+	stack_t *tmp = *stack;
 	int n = 0;
 
 	if ((tmp == NULL) || (tmp->next == NULL))
--- mod.c
+++ mod.c
@@ -8,7 +8,7 @@
 
 void mod(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = *stack;
+	stack_t *tmp = *stack;
 	int n = 0;
 
 	if ((tmp == NULL) || (tmp->next == NULL))

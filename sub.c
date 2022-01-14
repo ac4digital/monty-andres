e939c1129b9903972b734a69c3e45cf7272cb85c
Andrés Cataño <3866@holbertonschool.com>
2022-01-14 01:35:04 -0500

    New function sub

--- sub.c
+++ sub.c
@@ -14,7 +14,7 @@ void sub(stack_t **stack, unsigned int line_number)
 	if ((tmp == NULL) || (tmp->next == NULL))
 	{
 		fprintf(stderr,
-			"L%u: can't add, stack too short\n",
+			"L%u: can't sub, stack too short\n",
 			line_number);
 		exit(EXIT_FAILURE);
 	}
